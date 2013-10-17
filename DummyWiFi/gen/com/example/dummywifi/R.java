385384a08a4bda75f3fa955f4d919d1c2da5ea8c
James York <jtyork@ucsc.edu>
2013-10-17 03:01:52 -0700

    Revert "Revert "Converted fragment items to simpler view layout. App doesn't display rooms but should find them. Still needs to display list of available rooms.""
    This reverts commit 822a121362b625de983441dade49154bf720d5a1.

--- DummyWiFi/gen/com/example/dummywifi/R.java
+++ DummyWiFi/gen/com/example/dummywifi/R.java
@@ -21,10 +21,13 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_discover=0x7f020000;
+        public static final int ic_action_on_off=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int atn_direct_discover=0x7f080001;
+        public static final int atn_direct_enable=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,7 +38,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int discover_peers_button=0x7f050003;
+        public static final int enable_p2p_button=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int p2p_off_warning=0x7f050005;
     }
     public static final class style {
         /** 
